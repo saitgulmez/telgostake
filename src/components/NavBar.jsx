5cc187ffbb9b835fee4d8cbdc5d7f074e6f7273f
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2024-09-27 17:42:52 +0300

    Update Navbar.js file

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,18 +10,18 @@ const NavBar = () => {
   const navItem = "font-med text-xl px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900 "
 
   return (
-    <nav className="fixed flex justify-end space-x-4 items-center text-center w-[98%] h-24 mx-auto ">
+    <nav className="fixed flex justify-end space-x-4 border-b items-center text-center w-[98%] h-24 mx-auto">
       <img className="logo" src={logo} alt="" />
       <button
         data-collapse-toggle="navbar-default"
         type="button"
-        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        className=" inline-flex m-4 items-center p-2 w-20 h-15 justify-center text-sm  text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
         aria-controls="navbar-default"
         aria-expanded="false"
       >
         <span className="sr-only">Open main menu</span>
         <svg
-          className="w-5 h-5"
+          className="w-10 h-7 ml-2"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
