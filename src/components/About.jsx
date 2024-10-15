f1e44232d2f1983558075a0da37dca58bd3934f7
Mehmet Sait Gülmez <cenggulmez.65@gmail.com>
2024-10-15 03:22:26 +0300

    Remove comments

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,34 +5,34 @@ const About = () => {
     <div className="container px-5 py-24 mx-auto flex flex-wrap">
       <div className="h-full p-4 lg:w-1/3 ">
         <div className=" bg-gray-200 px-8 pt-16 pb-16 rounded hover:bg-gradient-to-r from-stone-500 to-sky-700	hover:text-white transition-all duration-300 ease-in-out">
-          <h2 className="text-xs mb-1">CATEGORY</h2>
-          <h1 className="title-font sm:text-2xl text-xl  mb-3">
+          {/* <h2 className="text-xs mb-1">CATEGORY</h2> */}
+          {/* <h1 className="title-font sm:text-2xl text-xl  mb-3">
             Raclette Blueberry Nextious
           </h1>
           <p className="mb-3">
             Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
             microdosing tousled waistcoat.
           </p>
-          <a className="text-indigo-500 inline-flex items-center">Learn More</a>
+          <a className="text-indigo-500 inline-flex items-center">Learn More</a> */}
         </div>
       </div>
 
       <div className="h-full p-4 lg:w-1/3">
         <div className="bg-gray-100 px-8 pt-16 pb-16 ">
-          <h2 className="text-xs mb-1">CATEGORY</h2>
-          <h1 className="title-font sm:text-2xl text-xl mb-3">
+          {/* <h2 className="text-xs mb-1">CATEGORY</h2> */}
+          {/* <h1 className="title-font sm:text-2xl text-xl mb-3">
             Selvage Poke Waistcoat
           </h1>
           <p className="mb-3">
             Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
             microdosing tousled waistcoat.
           </p>
-          <a className="text-indigo-500 inline-flex items-center">Learn More</a>
+          <a className="text-indigo-500 inline-flex items-center">Learn More</a> */}
         </div>
       </div>
       <div className="h-full p-4 lg:w-1/3">
         <div className="bg-gray-100 px-8 pt-16 pb-16 ">
-          <h2 className="text-xs mb-1">CATEGORY</h2>
+          {/* <h2 className="text-xs mb-1">CATEGORY</h2>
           <h1 className="title-font sm:text-2xl text-xl mb-3">
             Selvage Poke Waistcoat
           </h1>
@@ -40,7 +40,7 @@ const About = () => {
             Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
             microdosing tousled waistcoat.
           </p>
-          <a className="text-indigo-500 inline-flex items-center">Learn More</a>
+          <a className="text-indigo-500 inline-flex items-center">Learn More</a> */}
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,11 +17,7 @@ const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    // <nav className="bg-white border-gray-200 dark:bg-gray-900">
-    // <nav className="shadow-md w-full fixed top-0 left-0 ml-10">
-    // <nav className="md:container w-full fixed top-0 left-0 ">
     <nav className="w-full sticky top-0 left-0 mr-0 ml-0  ">
-      {/* <nav className="w-full fixed top-0 left-0 mr-0 ml-0  "> */}
       <div className="md:flex items-center  justify-between  py-4 md:px-10 px-7 bg-gradient-to-r from-stone-500 to-sky-700">
         {/* <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"> */}
         {/* Logo Part */}
@@ -48,11 +44,7 @@ const NavBar = () => {
             isOpen ? "top-20 bg-white h-[96rem] " : "top-[-490px] "
           }`}
         >
-          {/* <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 uppercase absolute md:static md:z-auto z-[-1] left-0 w-full h-screen md:h-auto md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            isOpen ? "top-20 bg-white " : "top-[-490px]"
-          }`}http://localhost:5173/about
-        > */}
+
           {Links.map((item) => (
             <li
               key={item.name}
@@ -61,11 +53,6 @@ const NavBar = () => {
             >
               <Link
                 to={item.link}
-                // href={link.link}
-                // className="text-gray-800 hover:text-gray-400  duration-500"
-                // "hover:underline hover:decoration-red-500 hover:decoration-2"
-                // className="font-bold text-sm px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900 "
-                // className="font-bold text-sm px-3 py-2 text-slate-700 md:text-white rounded-lg hover:bg-slate-100 hover:text-slate-900 "
                 className="font-bold text-sm px-3 py-2 text-slate-700 md:text-white rounded-lg  hover:text-slate-900 "
               >
                 {item.name}
@@ -74,56 +61,7 @@ const NavBar = () => {
           ))}
         </ul>
       </div>
-      {/* {isOpen && (
-        <div className="container px-5 md:py-24 py-10 mx-auto flex flex-wrap mt-40 md:mt-10" >
-          <div className="h-full p-4 lg:w-1/3 ">
-            <div className=" bg-gray-100 px-8 pt-16 pb-16 ">
-              <h2 className="text-xs mb-1">CATEGORY</h2>
-              <h1 className="title-font sm:text-2xl text-xl  mb-3">
-                Raclette Blueberry Nextious
-              </h1>
-              <p className="mb-3">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-              <a className="text-indigo-500 inline-flex items-center">
-                Learn More
-              </a>
-            </div>
-          </div>
 
-          <div className="h-full p-4 lg:w-1/3">
-            <div className="bg-gray-100 px-8 pt-16 pb-16 ">
-              <h2 className="text-xs mb-1">CATEGORY</h2>
-              <h1 className="title-font sm:text-2xl text-xl mb-3">
-                Selvage Poke Waistcoat
-              </h1>
-              <p className="mb-3">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-              <a className="text-indigo-500 inline-flex items-center">
-                Learn More
-              </a>
-            </div>
-          </div>
-          <div className="h-full p-4 lg:w-1/3">
-            <div className="bg-gray-100 px-8 pt-16 pb-16 ">
-              <h2 className="text-xs mb-1">CATEGORY</h2>
-              <h1 className="title-font sm:text-2xl text-xl mb-3">
-                Selvage Poke Waistcoat
-              </h1>
-              <p className="mb-3">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-              <a className="text-indigo-500 inline-flex items-center">
-                Learn More
-              </a>
-            </div>
-          </div>
-        </div>
-      )}   */}
 
     </nav>
   );
